bd2c71678fc71e9f3461555f2149cab140133e18
Tomás Niño Kehoe <tomas.nino@despegar.com>
2018-09-10 16:14:34 -0300

    Work with kafka 0.11.0.3

--- src/main/java/com/justone/kafka/sink/pg/json/PostgreSQLSinkConnector.java
+++ src/main/java/com/justone/kafka/sink/pg/json/PostgreSQLSinkConnector.java
@@ -29,102 +29,123 @@ package com.justone.kafka.sink.pg.json;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
 import org.apache.kafka.connect.connector.ConnectorContext;
 import org.apache.kafka.connect.connector.Task;
 import org.apache.kafka.connect.sink.SinkConnector;
+import org.apache.kafka.common.config.Config;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigValue;
 
 /**
  * Kafka sink connector for PostgreSQL
- * 
+ *
  * @author Duncan Pauly
  * @version 1.0
- * 
  */
 public class PostgreSQLSinkConnector extends SinkConnector {
-  /**
-   * Version of the connector
-   */
-  public final static String VERSION="1.0a"; 
-  /**
-   * Configuration properties for the connector
-   */
-  private Map<String,String> fProperties;
-  
-  /**
-   * Returns version of the connector
-   * @return version
-   */
-  @Override
-  public String version() {
-    
-      return VERSION;//return version
-      
-  }//version()
+    /**
+     * Version of the connector
+     */
+    public final static String VERSION = "1.0a";
+    /**
+     * Configuration properties for the connector
+     */
+    private Map<String, String> fProperties;
 
-  /**
-   * Initialise the connector
-   * @param ctx context of the connector
-   */
-  @Override
-  public void initialize(ConnectorContext ctx) {
-    //do nothing    
-  }//initialize()
-  
-  /**
-   * Initialise the connector
-   * @param ctx context of the connector
-   * @param taskConfigs task configuration
-   */
-  @Override
-  public void initialize(ConnectorContext ctx,
-              List<Map<String,String>> taskConfigs) {
-    //do nothing
-  }//initialize() 
+    private static final ConfigDef CONFIG_DEF = new ConfigDef();
 
-  /**
-   * Start the connector
-   * @param props connector configuration properties
-   */
-  @Override
-  public void start(Map<String, String> props) {
-    
-    fProperties=props;//set connector configuration properties
-    
-  }//start()
+    /**
+     * Returns version of the connector
+     *
+     * @return version
+     */
+    @Override
+    public String version() {
 
-  /**
-   * Stop the connector
-   */
-  @Override
-  public void stop() {
-    //do nothing
-  }//stop()
-  
-  /**
-   * Returns class of task
-   * @return class of task
-   */
-   @Override
-  public Class<? extends Task> taskClass() {
-      return PostgreSQLSinkTask.class;//return task class
-  }//taskClass()
+        return VERSION;//return version
 
-  /**
-   * Returns task configurations
-   * @param maxTasks maximum tasks to execute
-   * @return task configurations
-   */
-  @Override
-  public List<Map<String, String>> taskConfigs(int maxTasks) {
-    
-      ArrayList<Map<String, String>> configurations = new ArrayList<>();//construct list
-      
-      for (int i = 0; i < maxTasks; i++) {//for each task
-        configurations.add(fProperties);//add connector configuration
-      }//for each task
-      
-      return configurations;//return task configurations
-      
-  }//taskConfigs()
+    }//version()
+
+    /**
+     * Returns configdef of the connector
+     *
+     * @return CONFIG_DEF
+     */
+    public ConfigDef config() {
+        return CONFIG_DEF;
+    }
+
+    /**
+     * Initialise the connector
+     *
+     * @param ctx context of the connector
+     */
+    @Override
+    public void initialize(ConnectorContext ctx) {
+        //do nothing
+    }//initialize()
+
+    /**
+     * Initialise the connector
+     *
+     * @param ctx         context of the connector
+     * @param taskConfigs task configuration
+     */
+    @Override
+    public void initialize(ConnectorContext ctx,
+                           List<Map<String, String>> taskConfigs) {
+        //do nothing
+    }//initialize()
+
+    /**
+     * Start the connector
+     *
+     * @param props connector configuration properties
+     */
+    @Override
+    public void start(Map<String, String> props) {
+
+        fProperties = props;//set connector configuration properties
+
+    }//start()
+
+    /**
+     * Stop the connector
+     */
+    @Override
+    public void stop() {
+        //do nothing
+    }//stop()
+
+
+    /**
+     * Returns class of task
+     *
+     * @return class of task
+     */
+    @Override
+    public Class<? extends Task> taskClass() {
+        return PostgreSQLSinkTask.class;//return task class
+    }//taskClass()
+
+    /**
+     * Returns task configurations
+     *
+     * @param maxTasks maximum tasks to execute
+     * @return task configurations
+     */
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+
+        ArrayList<Map<String, String>> configurations = new ArrayList<>();//construct list
+
+        for (int i = 0; i < maxTasks; i++) {//for each task
+            configurations.add(fProperties);//add connector configuration
+        }//for each task
+
+        return configurations;//return task configurations
+
+    }//taskConfigs()
 
 }//PostgreSQLSinkConnector{}
